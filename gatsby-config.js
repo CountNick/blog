d907116fbbb20623406df861beb28262c07d0f4d
CountNick <meijer.nick1@gmail.com>
2021-01-28 18:59:26 +0100

    source from netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `blog`,
-        path: `${__dirname}/content/blog`,
+        path: `${__dirname}/blog`,
       },
     },
     `gatsby-transformer-remark`,
